5eb3750525fb634b5152096de1f5699679ad70a5
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2024-04-12 17:32:55 -0400

    feat - criar cliente e criar pedido

--- apps/pedidos/src/pedidos.service.ts
+++ apps/pedidos/src/pedidos.service.ts
@@ -1,49 +1,31 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
 import { PedidosDTO } from './dto/pedido.dto';
-import { StatusPedido } from '.prisma/client/PEDIDOS'
 import { ClientKafka } from '@nestjs/microservices';
 import { ClienteDTO } from './dto/cliente.dto';
+import { PedidosRepository } from './database/repositories/pedidos.repository';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class PedidosService {
   constructor(
-    private readonly prismaService: PrismaService,
+    private readonly pedidosRepository: PedidosRepository,
     @Inject('SERVIÇO_DE_PEDIDOS')
     private readonly kafkaService: ClientKafka
   ) {
 
   }
   async buscarPedidos() {
-    return await this.prismaService.pedidos.findMany()
-  }
-  async buscarCliente() {
-    return await this.prismaService.clientes.findMany()
-  }
-  async criarCliente(data: ClienteDTO) {
-    return await this.prismaService.clientes.create({
-      data: {
-        ...data
-      }
-    })
+    const pedidos = await this.pedidosRepository.listar()
+    await lastValueFrom(this.kafkaService.emit('pedidos', pedidos))
+    return pedidos
   }
 
-  async criarPedido(data: PedidosDTO) {
-    const user = await this.prismaService.clientes.findFirst({
-      where: {
-        id: data.clienteId
-      }
-    })
-    if (!user) throw new Error('Usuario n existe')
-    const pedido = await this.prismaService.pedidos.create({
-      data: {
-        ...data,
-        nome: user.nome,
-        status: StatusPedido.PENDENTE
-      }
-    })
-    this.kafkaService.emit('pedidos', 'PASSOU AQUI')
-    return pedido
-
+  async criarPedido(pedido: PedidosDTO) {
+    return await this.pedidosRepository.criarPedido(pedido)
   }
+  async criarCliente(cliente: ClienteDTO) {
+    return await this.pedidosRepository.criarCliente(cliente)
+  }
+
+
 }
